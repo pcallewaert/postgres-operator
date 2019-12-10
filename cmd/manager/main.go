b05d82c9c52f082de23e20e14075f86428e077d5
Pieter Callewaert <pieter@nephroflow.com>
2019-12-10 15:51:04 +0100

    update to operator-sdk v0.12 with go mod, had to disable metrics port however

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -16,7 +16,6 @@ import (
 	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
 	"github.com/operator-framework/operator-sdk/pkg/leader"
 	"github.com/operator-framework/operator-sdk/pkg/log/zap"
-	"github.com/operator-framework/operator-sdk/pkg/metrics"
 	"github.com/operator-framework/operator-sdk/pkg/restmapper"
 	sdkVersion "github.com/operator-framework/operator-sdk/version"
 	"github.com/spf13/pflag"
@@ -110,10 +109,10 @@ func main() {
 	}
 
 	// Create Service object to expose the metrics port.
-	_, err = metrics.ExposeMetricsPort(ctx, metricsPort)
-	if err != nil {
-		log.Info(err.Error())
-	}
+	// _, err = metrics.ExposeMetricsPort(ctx, metricsPort)
+	// if err != nil {
+	// 	log.Info(err.Error())
+	// }
 
 	log.Info("Starting the Cmd.")
 
